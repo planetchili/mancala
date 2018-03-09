e349c536a1081a43142d38ed2e0e0bac670bf7a6
chili <ray_kun@hotmail.com>
2018-03-09 14:09:08 +0900

    better div hiding

--- main.ts
+++ main.ts
@@ -1,19 +1,12 @@
 import * as $ from "jquery";
 import Globals from "./MancalaGlobals";
 import LoginWindow from "./LoginWindow";
-import AccountWidget from "./AccountWidget";
 
 
 $(document).ready(() =>
 {
 	// hide all popup overlays
-	$("#login-overlay").hide();
-	$("#room-overlay").hide();
-	$("#create-overlay").hide();
-	$("#join-overlay").hide();
-	$("#game-overlay").hide();
-	$("#result-overlay").hide();
-	$("#forfeit-overlay").hide();
+	$("div.overlay").hide();
 
 	Globals.Init();
 
